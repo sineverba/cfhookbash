87b065c7913c928aefe10eccf14bd03a4cc634aa
sineverba <2634842+sineverba@users.noreply.github.com>
2018-06-12 17:16:54 +0200

    Better pseudocode

--- hook.sh
+++ hook.sh
@@ -14,14 +14,18 @@ deploy_challenge() {
     	"www.example.com")
     	
     		token="t0k3n"
+    		zones="z0n35"
     		echo "$token"
+    		echo "$zones"
     		
     	;;
     	
     	"www.example.net")
     	
     		token="4n0th3rt0k3n"
+    		zones="4n0th3rz0n35"
     		echo "$token"
+    		echo "$zones"
     		
     	;;
     
