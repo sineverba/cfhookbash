b43ea243137e79f240188613cb744d4743b3a463
sineverba <2634842+sineverba@users.noreply.github.com>
2018-06-13 07:00:05 +0200

    Set right variables

--- config.default.sh
+++ config.default.sh
@@ -2,15 +2,17 @@ case ${1} in
     	
     	"www.example.com")
     	
-    		token="t0k3n"
+    		global_api_key="t0k3n"
     		zones="z0n35"
+    		eamil="info@example.com"
     		
     	;;
     	
     	"www.example.net")
     	
-    		token="4n0th3rt0k3n"
+    		global_api_key="4n0th3rt0k3n"
     		zones="4n0th3rz0n35"
+    		email="cert@example.net"
     		
     	;;
     
