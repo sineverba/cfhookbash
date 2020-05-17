8fc1aabc994e3f39370ceb0571824bfc53ed6fdc
sineverba <alessandro.arici@gmail.com>
2020-05-17 20:21:43 +0200

    Fix Docker paths on tests

--- .deploy/scripts/test.sh
+++ .deploy/scripts/test.sh
@@ -8,6 +8,6 @@ docker run -dit \
     -v ${TRAVIS_BUILD_DIR}/docker/app/certs:/certs --name ${DOCKER_IMAGE} \
     --entrypoint=/bin/sh ${DOCKER_USERNAME}/${DOCKER_IMAGE}
 docker exec -it ${DOCKER_IMAGE} cat /dehydrated/dehydrated | grep 0.6.5
-docker inspect -f {{.Config.Labels}} ${DOCKER_IMAGE} | grep $VCS_REF
+#docker inspect -f {{.Config.Labels}} ${DOCKER_IMAGE} | grep $VCS_REF
 docker container stop ${DOCKER_IMAGE}
 docker container rm ${DOCKER_IMAGE}
