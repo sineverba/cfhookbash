fd5e370c0ee1669e681da5763e197313d206c7f3
sineverba <alessandro.arici@gmail.com>
2020-05-17 19:56:27 +0200

    Fix Docker paths on tests

--- .deploy/scripts/test.sh
+++ .deploy/scripts/test.sh
@@ -2,9 +2,9 @@
 
 set -e
 docker run -dit \
-    -v ./docker/app/config:/config \
-    -v ./docker/app/dehydrated:/dehydrated \
-    -v ./docker/app/certs:/certs --name ${DOCKER_IMAGE} \
+    -v /docker/app/config:/config \
+    -v /docker/app/dehydrated:/dehydrated \
+    -v /docker/app/certs:/certs --name ${DOCKER_IMAGE} \
     --entrypoint=/bin/sh ${DOCKER_USERNAME}/${DOCKER_IMAGE}
 docker exec -it ${DOCKER_IMAGE} cat /dehydrated/dehydrated | grep 0.6.5
 docker inspect -f {{.Config.Labels}} ${DOCKER_IMAGE} | grep $VCS_REF
