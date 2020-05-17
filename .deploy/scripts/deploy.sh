c0e11e5c779542413b78ff92394f615b3fca4bac
sineverba <alessandro.arici@gmail.com>
2020-05-17 21:19:35 +0200

    Add Label to Dockerfile. Add deploy Docker Hub script

---
+++ .deploy/scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker tag ${DOCKER_USERNAME}/${DOCKER_IMAGE} ${DOCKER_USERNAME}/${DOCKER_IMAGE}:latest
+docker tag ${DOCKER_USERNAME}/${DOCKER_IMAGE} ${DOCKER_USERNAME}/${DOCKER_IMAGE}:${TRAVIS_TAG}
+
+echo "$DOCKER_API_KEY" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+docker push ${DOCKER_USERNAME}/${DOCKER_IMAGE}:latest;
+docker push ${DOCKER_USERNAME}/${DOCKER_IMAGE}:$TRAVIS_TAG;
