f95626d94a3a38527e48536fd6822d5ee8b9e050
sineverba <alessandro.arici@gmail.com>
2020-05-17 19:53:01 +0200

    Add Docker and Travis

---
+++ .deploy/scripts/test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+docker run -dit \
+    -v ./docker/app/config:/config \
+    -v ./docker/app/dehydrated:/dehydrated \
+    -v ./docker/app/certs:/certs --name ${DOCKER_IMAGE} \
+    --entrypoint=/bin/sh ${DOCKER_USERNAME}/${DOCKER_IMAGE}
+docker exec -it ${DOCKER_IMAGE} cat /dehydrated/dehydrated | grep 0.6.5
+docker inspect -f {{.Config.Labels}} ${DOCKER_IMAGE} | grep $VCS_REF
+docker container stop ${DOCKER_IMAGE}
+docker container rm ${DOCKER_IMAGE}
---
+++ TODO.md
@@ -0,0 +1,8 @@
+# Docker
++ Add labels to Dockerfile
++ Add Microbadger
++ Add Docker example
++ Add cronjob
++ Remove folder dehydrated as volume, if possible
++ Add Force Flag as environment on Docker start
++ Add docker-compose example
